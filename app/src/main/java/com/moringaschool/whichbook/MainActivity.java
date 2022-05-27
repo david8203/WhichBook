cf51510b669919fd534f5a4ac79969193ec90f9c
David <kingdice@Atomic>
2022-05-27 22:00:14 +0300

    here we are creating a layout file and giving it buttons for our first activity

---
+++ app/src/main/java/com/moringaschool/whichbook/MainActivity.java
@@ -0,0 +1,18 @@
+package com.moringaschool.whichbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnSeeAll,btnCurrentReading,btnWantToRead,btnAlreadyRead,btnAbout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
+
