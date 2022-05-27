67313621359122bddfc7058c8c5ac2528b6b1896
David <kingdice@Atomic>
2022-05-27 22:38:24 +0300

    here we are adding a bindview in our main activity

--- app/src/main/java/com/moringaschool/whichbook/MainActivity.java
+++ app/src/main/java/com/moringaschool/whichbook/MainActivity.java
@@ -5,13 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import butterknife.BindView;
+
 public class MainActivity extends AppCompatActivity {
-    private Button btnSeeAll,btnCurrentReading,btnWantToRead,btnAlreadyRead,btnAbout;
+    @BindView(R.id.btnAllBooks) Button mBtnAllBooks;
+    @BindView(R.id.btnCurrentlyReading) Button mBtnCurrentlyReading;
+    @BindView(R.id.btnWantTo) Button mBtnWantTo;
+    @BindView(R.id.btnAlreadyRead) Button mBtnAlreadyRead;
+    @BindView(R.id.btnAbout) Button mBtnAbout;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
     }
 }
