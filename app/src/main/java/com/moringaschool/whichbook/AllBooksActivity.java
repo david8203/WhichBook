c77f1765d8d763618f527c5d8e8d75e6b5ffcf4a
David <kingdice@Atomic>
2022-05-28 13:33:14 +0300

    here we create a loog in our all books activity and adds an arrayList.

--- app/src/main/java/com/moringaschool/whichbook/AllBooksActivity.java
+++ app/src/main/java/com/moringaschool/whichbook/AllBooksActivity.java
@@ -3,12 +3,21 @@ package com.moringaschool.whichbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class AllBooksActivity extends AppCompatActivity {
-
+    private static final String TAG = "AllBooksActivity";
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_books);
+        Log.d(TAG, "onCreate: started");
+        ListView listView = (ListView) findViewById(R.id.booksListView);
+//here we define an arrayList for our strings and the adapter
+        ArrayList<String> books = new ArrayList<>();
     }
 }
