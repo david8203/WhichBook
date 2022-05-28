8af36a911d6459ca6c090b60a85e94bb4f6071c4
David <kingdice@Atomic>
2022-05-28 22:35:51 +0300

    here we create a recycler view to replace our list view and creates a custom layout file for our adapter

--- app/src/main/java/com/moringaschool/whichbook/AllBooksActivity.java
+++ app/src/main/java/com/moringaschool/whichbook/AllBooksActivity.java
@@ -1,23 +1,26 @@
 package com.moringaschool.whichbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class AllBooksActivity extends AppCompatActivity {
     private static final String TAG = "AllBooksActivity";
-    private ListView listView;
+    private RecyclerView booksRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_books);
         Log.d(TAG, "onCreate: started");
-        ListView listView = (ListView) findViewById(R.id.booksListView);
-//here we define an arrayList for our strings and the adapter
-        ArrayList<String> books = new ArrayList<>();
+
+        booksRecyclerView = (RecyclerView) findViewById(R.id.booksRecyclerView);
+
     }
 }
