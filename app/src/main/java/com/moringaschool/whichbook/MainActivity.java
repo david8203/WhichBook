88f06c39f1cfe5ca4e9c12d41518f742ae9f73dd
David <kingdice@Atomic>
2022-05-28 13:18:11 +0300

    we asjust some layouts in our main xml files and create an intent in our main activity to take us to our books directory.

--- app/src/main/java/com/moringaschool/whichbook/MainActivity.java
+++ app/src/main/java/com/moringaschool/whichbook/MainActivity.java
@@ -2,26 +2,38 @@ package com.moringaschool.whichbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.btnAllBooks) Button mBtnAllBooks;
-    @BindView(R.id.btnCurrentlyReading) Button mBtnCurrentlyReading;
-    @BindView(R.id.btnWantTo) Button mBtnWantTo;
-    @BindView(R.id.btnAlreadyRead) Button mBtnAlreadyRead;
-    @BindView(R.id.btnAbout) Button mBtnAbout;
+private Button btnSeeAll,btnCurrentlyReading,btnWantToRead,btnAlreadyRead,btnAbout;
 
-
-
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//a method to initialize our buttons
+        initWidgets();
+        setOnClickListeners();
+    }
+    private void setOnClickListeners(){
+        btnSeeAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            Intent intent = new Intent(MainActivity.this,AllBooksActivity.class);
+            startActivity(intent);
+            }
+        });
+    }
+    private void initWidgets() {
+        btnSeeAll = (Button) findViewById(R.id.btnAllBooks);
+        btnCurrentlyReading = (Button) findViewById(R.id.btnCurrentlyReading);
+        btnWantToRead = (Button) findViewById(R.id.btnWantToRead);
+        btnAlreadyRead = (Button) findViewById(R.id.btnAlreadyRead);
+        btnAbout = (Button) findViewById(R.id.btnAbout);
 
     }
 }
